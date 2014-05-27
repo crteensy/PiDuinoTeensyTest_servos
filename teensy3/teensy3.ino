2cf2b2dc5dfe8afb258b80ba20fabd0e06071482
crteensy <webmaster@avrbeginners.net>
2014-05-27 20:16:01 +0200

    updated teensy sketch

--- teensy3/teensy3.ino
+++ teensy3/teensy3.ino
@@ -2,12 +2,50 @@
 
 #include <PulsePosition.h>
 
+static const uint8_t slaveAddress = 0x20;
+
+static const uint8_t numChannels = 8;
+typedef uint16_t servoValueType;
+static servoValueType slaveReceiveBuffer[numChannels];
+
+PulsePositionOutput ppmOut;
+
+void rxFun(size_t len)
+{
+  if (len == sizeof(servoValueType)*numChannels)
+  {
+    for (size_t i = 0; i < numChannels; i++)
+    {
+      uint16_t val = (((Wire.readByte() & 0xFF) << 8) | (Wire.readByte() & 0xFF));
+      slaveReceiveBuffer[i] = val;
+      ppmOut.write(i+1, 1000. + 1000.*val/65535.);
+    }
+    return;
+  }
+
+  if (len == (1 + sizeof(servoValueType)))
+  {
+    uint8_t channel = Wire.readByte();
+    if (channel >= numChannels)
+    {
+      // error: channel doesn't exist
+      return;
+    }
+    uint16_t val = (((Wire.readByte() & 0xFF) << 8) | (Wire.readByte() & 0xFF));
+    slaveReceiveBuffer[channel] = val;
+    ppmOut.write(channel+1, 1000. + 1000.*val/65535.);
+  }
+}
+
 void setup()
 {
-  // initialize i2c slave
+  Wire.begin(I2C_SLAVE, slaveAddress, I2C_PINS_16_17, I2C_PULLUP_EXT, I2C_RATE_400);
+  Wire.onReceive(rxFun);
+
+  ppmOut.begin(5);  // connect pins 5 and 6 together...
 }
 
 void loop()
 {
-  // 
 }
+
